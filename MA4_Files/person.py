3c72039d7ad7889fa0e83402587aa877ba48616e
vivols <115640894+vivols@users.noreply.github.com>
2022-10-21 15:47:13 +0200

    tre

--- MA4_Files/person.py
+++ MA4_Files/person.py
@@ -10,6 +10,8 @@ class Person(object):
 		lib.Person_get.restype = ctypes.c_int
 		lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
 		lib.Person_delete.argtypes = [ctypes.c_void_p]
+		lib.Person_fib.argtypes =[ctypes.c_void_p] 
+		lib.Person_fib.restype = ctypes.c_int
 		self.obj = lib.Person_new(age)
 
 	def get(self):
@@ -17,6 +19,9 @@ class Person(object):
 
 	def set(self, age):
 		lib.Person_set(self.obj, age)
+
+	def fib(self): 
+		lib.Person_fib(self.obj)
         
 	def __del__(self):
 		return lib.Person_delete(self.obj)
