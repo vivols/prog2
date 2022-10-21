d6bab3e5e7a5958bd346bf6768506d00b562d795
vivols <115640894+vivols@users.noreply.github.com>
2022-10-21 14:54:16 +0200

    hm

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 from time import perf_counter as pc
 
 def main():
+
+	'''def fib_py(n):
+		if n <= 1:
+			return n
+		else:
+			return(fib_py(n-1) + fib_py(n-2))'''
+
+
 	f = Person(5)
 	print(f.get())
 	f.set(7)
