2a1d4bc7f558be17db8d8ba74c1c831c30a82c65
vivols <115640894+vivols@users.noreply.github.com>
2022-10-21 16:02:12 +0200

    up

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -3,22 +3,32 @@
 from person import Person
 import matplotlib.pyplot as plt
 from time import perf_counter as pc
+from numba import njit 
+
 
 def main():
 
-	'''def fib_py(n):
+	def fib_py(n):
 		if n <= 1:
 			return n
 		else:
-			return(fib_py(n-1) + fib_py(n-2))'''
+			return(fib_py(n-1) + fib_py(n-2))
+	
+	@njit 
+	def fib_numba(n):
+		if n <= 1:
+			return n
+		else:
+			return(fib_numba(n-1) + fib_numba(n-2))
+	
 
 
 	f = Person(5)
 	print(f.get())
 	print(f.fib()) 
-	f.set(7)
-	print(f.get())
-	print(f.fib())
+	
+	print(fib_py(8))
+	print(fib_numba(7))
 
 if __name__ == '__main__':
 	main()
